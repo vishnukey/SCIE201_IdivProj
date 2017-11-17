d9302208b0aede17ab7bc61b5c68d52937d6aa43
vishnukey <vishnukey@gmail.com>
2017-11-17 15:03:39 -0700

    prettied up the piecharts

--- visualizations/pieCharts/mkPieAge.R
+++ visualizations/pieCharts/mkPieAge.R
@@ -4,16 +4,18 @@ png(file = "ages.jpg")
 
 data = read_json("../../data/censusPretty_newGeom.geojson", simplifyVector = TRUE)
 ages <- c(data$totals$'15_19',data$totals$'5_14')
+colours <- c("#55FF55", "#5555FF")
 pie(ages,
         labels = as.character(ages),
         radius = 1,
-        main = "Age distribution of children",
-        col = rainbow(length(ages)))
+        main = "Age distribution of children in Calgary",
+        col = colours
+)
 
 legend(
         "bottomright",
         as.character(c('14_19', '5_14')),
         cex = 0.8,
-        fill = rainbow(length(ages))
+        fill = colours
 )
 dev.off()
--- visualizations/pieCharts/mkPieMF.R
+++ visualizations/pieCharts/mkPieMF.R
@@ -4,18 +4,19 @@ png(file = "genders.jpg")
 
 data = read_json("../../data/censusPretty_newGeom.geojson", simplifyVector = TRUE)
 genders <- c(data$totals$'male',data$totals$'fem')
+colours <- c('#5555FF', '#FF6666')
 pie(genders,
         labels = as.character(genders),
         radius = 1,
-        main = "Age distribution of children",
+        main = "Gender distribution of children in Calgary",
         #col = rainbow(length(genders)))
-        col = c('#0000FF', '#FF0000')
+        col = colours
 )
 
 legend(
         "bottomright",
         as.character(c('male', 'female')),
         cex = 0.8,
-        fill = c('#0000FF', '#FF0000')
+        fill = colours
 )
 dev.off()
